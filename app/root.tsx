e966b511a96f49273ae7f09357457791b8c901f3
Tim Morton <tim@timothymorton.com>
2024-02-12 10:07:39 -0500

    Added basic layout from theme

--- app/root.tsx
+++ app/root.tsx
@@ -7,18 +7,23 @@ import {
 } from "@remix-run/react";
 
 import "./tailwind.css"; // injects CSS into the page as inline styles (from Vite)
+import { Layout } from "./components/Layout";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full antialiased">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <div className="flex w-full">
+          <Layout>
+            <Outlet />
+          </Layout>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
