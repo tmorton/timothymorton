936829c69da825a9e4327f51eae476d068f151d2
Tim Morton <tim@timothymorton.com>
2024-02-13 09:55:17 -0500

    Tweaked home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,10 +7,13 @@ export default function Index() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Engineering for Humans
+            Timothy Morton
           </h1>
+          <h2 className="text-3xl  tracking-tight text-zinc-800 sm:text-4xl dark:text-zinc-100">
+            Engineering for humans
+          </h2>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            Hey, I'm Tim, a <strong>product focused</strong> and{" "}
+            Hello, I'm Tim. I'm a <strong>product focused</strong> and{" "}
             <strong>full stack</strong> software engineer based in the
             Washington, DC area. I'm currently seeking a senior+ role in a
             product focused company.
